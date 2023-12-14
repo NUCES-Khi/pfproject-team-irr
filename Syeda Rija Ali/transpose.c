bd3de8ff50f66e2469a9519364a6462d9605b29b
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2023-12-14 18:38:09 +0500

    Update transpose.c

--- Syeda Rija Ali/transpose.c
+++ Syeda Rija Ali/transpose.c
@@ -1,7 +1,13 @@
+/*
+The code below is used for transposing a matrix using dynamically allocated memory.
+*/
+
 #include <stdio.h>
+#include<stdlib.h>
+
 
 //function to take input and display the matrix
-void InputM(int m, int n, int matrix[m][n]) {
+void InputM(int m, int n, int **matrix) {
   
     printf("Enter values of the Matrix you want to Transpose: \n");
     for(int i=0;i<m;i++){//taking inputs of  matrix
@@ -9,6 +15,7 @@ void InputM(int m, int n, int matrix[m][n]) {
             scanf("%d",&matrix[i][j]);
         }
     }
+
 //printing the user-input matrix
     printf("The Matrix is:");
     printf("\n");
@@ -22,7 +29,7 @@ void InputM(int m, int n, int matrix[m][n]) {
 }
 
 //function for transposing a matrix
-void TransposeM(int m, int n, int matrix[m][n]){
+void TransposeM(int m, int n, int **matrix){
 
  printf("Transpose of the matrix is:\n");
      for(int i=0; i<n; i++) {
@@ -42,11 +49,23 @@ int m,n;
     printf("Enter number of columns of Matrix: ");
     scanf("%d",&n);
 
-int matrix[m][n]; 
+// Dynamically allocate memory for the matrix
+    int **matrix = (int **)malloc(m * sizeof(int *));
+    for (int i = 0; i < m; i++) {
+        matrix[i] = (int *)malloc(n * sizeof(int));
+    }
+
+   //int matrix[m][n]; 
 
         InputM(m,n,matrix);
         TransposeM(m,n,matrix);
 
+    // Free dynamically allocated memory
+    for (int i = 0; i < m; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
 
+    return 0;
 
 }
