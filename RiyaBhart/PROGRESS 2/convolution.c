a4181fa7bd4f9639b038b19f1e38792046cbdfc3
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-12-17 21:28:21 +0500

    kernel issue sorted!

--- RiyaBhart/PROGRESS 2/convolution.c
+++ RiyaBhart/PROGRESS 2/convolution.c
@@ -4,33 +4,31 @@ void fillMatrix(int matrix[][100], int n) {
     printf("Enter the matrix elements:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (scanf("%d", &matrix[i][j]) != 1) {
-                printf("Invalid input. Please enter a valid integer.\n");
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+void fillKernel(int kernel[][100], int n) {
+    printf("Enter the kernel matrix:\n");
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = 0; j < n - 2; j++) {
+            scanf("%d", &kernel[i][j]);
+            if (kernel[i][j] != 0 && kernel[i][j] != 1 && kernel[i][j] != -1) {
+                printf("Invalid input. Please enter a valid integer (0, 1, or -1).\n");
                 return;
             }
         }
     }
 }
 
-void fillKernel(int kernel[][3]) {
-    printf("Enter the 3x3 kernel matrix:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (scanf("%d", &kernel[i][j]) != 1) {
-                printf("Invalid input. Please enter a valid integer.\n");
-                return;
-            }
-        }
-    }
-}
-
-void convolution(int matrix[][100], int kernel[][3], int n) {
+void convolution(int matrix[][100], int kernel[][100], int n) {
     if (n < 3) {
         printf("Matrix size should be at least 3x3 for convolution.\n");
         return;
     }
 
-    int result[n - 2][n - 2];
+    int result[100][100];
 
     for (int i = 0; i < n - 2; i++) {
         for (int j = 0; j < n - 2; j++) {
@@ -38,7 +36,6 @@ void convolution(int matrix[][100], int kernel[][3], int n) {
 
             for (int x = 0; x < 3; x++) {
                 for (int y = 0; y < 3; y++) {
-                    
                     int matrixRow = i + x;
                     int matrixCol = j + y;
 
@@ -66,8 +63,8 @@ int main() {
     int matrix[100][100];
     fillMatrix(matrix, n);
 
-    int kernel[3][3];
-    fillKernel(kernel);
+    int kernel[100][100];
+    fillKernel(kernel, n);
 
     printf("Convolution result:\n");
     convolution(matrix, kernel, n);
